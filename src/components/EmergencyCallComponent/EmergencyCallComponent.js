968ca7c336d625b9004b71fa4c6d0abd515db676
Sanjay  Yepuri <sanjayyepuri@wireless-10-148-124-104.public.utexas.edu>
2018-03-31 18:27:17 -0500

    Scratched adding icon to button

--- src/components/EmergencyCallComponent/EmergencyCallComponent.js
+++ src/components/EmergencyCallComponent/EmergencyCallComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableHighlight } from 'react-native';
 import { RkStyleSheet } from 'react-native-ui-kitten';
 import call from 'react-native-phone-call';
 import Icon from 'react-native-vector-icons/Foundation'
@@ -22,19 +22,14 @@ export class EmergencyCallComponent extends React.Component {
 
     render() {
         return (
-            <View style = {styles.container} >
+
                 <View style = {styles.buttonContainer} >
-                    <Icon 
-                        name = "telephone" size = {15} color = "#000000" 
-                    />
                     <Button
                         onPress = {this._onPressButton}
-                        title = "Call 911"
-                        size = '20'
+                        title="Call 911"           
                     >
                     </Button>
                 </View>
-            </View>
         );
     }
 }
@@ -42,7 +37,7 @@ export class EmergencyCallComponent extends React.Component {
 let styles = RkStyleSheet.create(theme => ({
     container: {
         flex: 1,
-        justifyContent = 'center',
+        justifyContent: 'center'
     },
     buttonContainer: {
         margin: 15

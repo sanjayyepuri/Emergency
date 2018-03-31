451888eb931dd43c2101c3b3801604935d3e6ea2
Helen <helen_zhou@live.com>
2018-03-31 18:12:22 -0500

    dead

--- src/components/EmergencyCallComponent/EmergencyCallComponent.js
+++ src/components/EmergencyCallComponent/EmergencyCallComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { RkStyleSheet } from 'react-native-ui-kitten';
 import call from 'react-native-phone-call';
+import Icon from 'react-native-vector-icons/Foundation'
 
 const callER = {
     number: '911 ',
@@ -21,20 +22,29 @@ export class EmergencyCallComponent extends React.Component {
 
     render() {
         return (
-            <Button
-                onPress = {this._onPressButton}
-                title = "Call an Emergency Hotline"
-            >
-            </Button>
+            <View style = {styles.container} >
+                <View style = {styles.buttonContainer} >
+                    <Icon 
+                        name = "telephone" size = {15} color = "#000000" 
+                    />
+                    <Button
+                        onPress = {this._onPressButton}
+                        title = "Call 911"
+                        size = '20'
+                    >
+                    </Button>
+                </View>
+            </View>
         );
     }
 }
 
 let styles = RkStyleSheet.create(theme => ({
-    root: {
-      backgroundColor: theme.colors.screen.base
+    container: {
+        flex: 1,
+        justifyContent = 'center',
     },
-    title: {
-      marginBottom: 5
-    },
+    buttonContainer: {
+        margin: 15
+    }
   }));

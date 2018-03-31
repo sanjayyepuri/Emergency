e1fa9143a8d64bc2d8a83cca6834335da7c58fff
Sanjay  Yepuri <sanjayyepuri@wireless-10-148-124-104.public.utexas.edu>
2018-03-31 17:47:15 -0500

    Renamed File

---
+++ sample-ers.js
@@ -0,0 +1,30 @@
+export default sanmple_er = { data: 
+    [
+        {
+            name: "Seton Medical Center Austin: Emergency Room",
+            address: "1201 W 38th St",
+            phone: "asdf"
+        },
+        {
+            name: "Heart Hospital of Austin",
+            address: "3801 N Lamar Blvd",
+            phone: "asdf"
+        },
+        {
+            name: "Neighbors Emergency Center - Austin",
+            address: "1801 East 51st Street, Bldg. H",
+            phone: "asdf"
+        },
+        {
+            name: "First Choice Emergency Room",
+            address: "2020 E Riverside Dr",
+            phone: "asdf"
+        },
+        {
+            name: "First Choice Emergency Room",
+            address: "4015 S Lamar Blvd",
+            phone: "asdf"
+        },
+
+    ]
+}
